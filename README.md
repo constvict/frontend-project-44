e7e7f47f394aabd9f2a4e557b1da14d52fbf0672
constvict <constantinumnov@gmail.com>
2022-11-14 18:47:35 +0700

    update readme/eslitrc/package add brain-even game

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/constvict/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/constvict/frontend-project-44/actions)
+### Codeclimate:
+[![Maintainability](https://api.codeclimate.com/v1/badges/316e9e6f065340b6cc19/maintainability)](https://codeclimate.com/github/constvict/frontend-project-44/maintainability)
+### Brain-even:
+[![asciicast](https://asciinema.org/a/WWBYu0YQG1M4x57ViRJzXphH2.svg)](https://asciinema.org/a/WWBYu0YQG1M4x57ViRJzXphH2)
---
+++ bin/brain-even.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
+
+const isEven = (num) => {
+    return num % 2 === 0;
+}
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".')
+
+for (let i = 0; i < 3; i ++) {
+    let getRandom = Math.floor(Math.random() * 10) + 1;
+    const answer = isEven(getRandom) ? 'yes' : 'no';
+    console.log('Question: ' + getRandom);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer != answer) {
+        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+        console.log(`Let's try again, ${userName}!`);
+        break
+    } else {
+        console.log('Correct!');
+    }
+
+    if (i === 2) {
+        console.log(`Congratulations, ${userName}!`);
+    }
+}
